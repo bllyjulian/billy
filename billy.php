c51d77ccd721516a7238f7596efa513348893850
Bllyy <124893821+bllyjulian@users.noreply.github.com>
2023-08-30 15:10:35 +0700

    Add files via upload

---
+++ billy.php
@@ -0,0 +1,9 @@
+<?php
+    echo "Hallo, Nama saya Billy Mohammad<br>";
+
+    $angka1 = 6;
+    $angka2 = 2;
+    $hasil = $angka1 + $angka2;
+
+    echo "Hasil pertambahan $angka1 + $angka2 = $hasil";
+    ?>
